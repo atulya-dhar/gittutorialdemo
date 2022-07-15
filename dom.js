59f41785bda8c160bf431b5c92979ec37b3da3a7
Atulya Dhar <atulyadhar0428@gmail.com>
2022-07-15 18:11:53 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,177 @@
+//Examine the document object 
+//console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123
+// console.log(document.doctype);   
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);   
+// console.log(document.all[10]); 
+// document.all[10].textContent = "Hello";
+// console.log(documenr.forms[0]);
+// console.log(document.links);
+//console.log(document.images);
+
+//GetElementByID
+
+//console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+//console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+//console.log(headerTitle.innerText);
+//headerTitle.innerHTML = '<h3>Hello</h3>';
+//header.style.borderBottom = 'solid 3px #000';
+
+// GetElementsByClassName //
+// var li = document.getElementsByClassName('list-group-item');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.background = 'green';
+
+// Gives Error
+//li.style.backgroundColor = '#f4f4f4';
+
+//   for(var i = 0; i < li.length; i++)
+//   {
+//     li[i].style.backgroundColor = '#f4f4f4';
+//     li[2].style.backgroundColor = 'green';
+//     li[i].style.fontWeight = 'bold';
+//   }
+
+// GetElementsByTagName
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.background = 'green';
+
+// Gives Error
+// li.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < li.length; i++)
+// {
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
+
+// QuerySelector //
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 20px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND";
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'green';
+
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.color = 'white';
+
+// QuerySelectorAll; //
+// var titles = document.querySelectorAll('.title');
+
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+
+// var odd = document.querySelectorAll('li:nth-child(odd');
+
+// for(var i = 0; i< odd.length; i++)
+// {
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+
+
+/* Traversing The Dom */
+var itemList = document.querySelector('#items');
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// childNodes
+//console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// FirstChild                           // gives the blank text space
+// console.log(itemList.firstChild);
+
+// firstElementChild                     
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+
+// nextSibling
+//console.log(itemList.nextSibling);
+
+// nextElementSibling
+//console.log(itemList.nextElementSibling);
+
+// previousSibling
+//console.log(itemList.previousSibling);
+// previousElementSibling
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color = 'green';
+
+
+// CreateElement
+
+//Create a div
+var newDiv = document.createElement('div');
+
+// Add a class
+newDiv.className = 'hello';
+
+// Add id
+newDiv.id = 'hello1';
+
+// Add attr
+
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create text node
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
+
+var newtag = document.createElement('li') 
+
+var newtag_1 = document.createTextNode('Hello World');
+
+
+var parentnode = document.getElementById('items');
+
+parentnode.innerHTML = "<li>hello world</li>" + parentnode.innerHTML;
